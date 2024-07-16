39db24db2488862275aae6a77fc13c9a5a4068d2
Wong Tsang Han <tsanghan@gmail.com>
2024-07-16 21:39:45 +0800

    Initial commit

---
+++ sctp-ce6-mod2.16-cdk-go_test.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+// 	"testing"
+
+// 	"github.com/aws/aws-cdk-go/awscdk/v2"
+// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+// 	"github.com/aws/jsii-runtime-go"
+// )
+
+// example tests. To run these tests, uncomment this file along with the
+// example resource in sctp-ce6-mod2.16-cdk-go_test.go
+// func TestSctpCe6Mod216CdkGoStack(t *testing.T) {
+// 	// GIVEN
+// 	app := awscdk.NewApp(nil)
+
+// 	// WHEN
+// 	stack := NewSctpCe6Mod216CdkGoStack(app, "MyStack", nil)
+
+// 	// THEN
+// 	template := assertions.Template_FromStack(stack, nil)
+
+// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+// 		"VisibilityTimeout": 300,
+// 	})
+// }
